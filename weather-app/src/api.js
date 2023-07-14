dadb4b10744638b018d072ab5b3f9c15dd4ac0ba
srewashimondal <67836280+srewashimondal@users.noreply.github.com>
2023-07-14 11:58:34 -0400

    Update api.js

--- weather-app/src/api.js
+++ weather-app/src/api.js
@@ -1,16 +1,24 @@
 const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions';
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '71a0752e28msh084cebbe004a0aep180adejsndcc331acc048',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-	}
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '71a0752e28msh084cebbe004a0aep180adejsndcc331acc048',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+  }
 };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
+const fetchData = async () => {
+  try {
+    const response = await fetch(url, options);
+    if (response.ok) {
+      const result = await response.text();
+      console.log(result);
+    } else {
+      throw new Error('Request failed with status: ' + response.status);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+fetchData();
