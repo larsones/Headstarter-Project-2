82ee510d61af4f38f47dd68d15db5a6ef3df0f21
srewashimondal <67836280+srewashimondal@users.noreply.github.com>
2023-07-14 14:54:53 -0400

    Update search.js

--- weather-app/src/components/search/search.js
+++ weather-app/src/components/search/search.js
@@ -1,5 +1,23 @@
-const Search = () => {
-  return ('Hello')
+import {AsyncPaginate } from "react-select-asynch-paginate";
+
+const Search = (onSearchChange) => {
+
+  const [search, setSearch] = useState(null);
+
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+    
+  }
+  
+  return (
+    <AsyncPaginate
+    placeholder="Search for city"
+    debounceTimeout={600}
+    value={search}
+    onChange={handleOnChange}
+    />
+  )
 }
 
 export default Search;
