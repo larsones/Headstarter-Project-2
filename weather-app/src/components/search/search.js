9d03ffd030cae5f8b55a6c7cf3b43186c9b33e62
srewashimondal <67836280+srewashimondal@users.noreply.github.com>
2023-07-14 15:09:55 -0400

    Update search.js

--- weather-app/src/components/search/search.js
+++ weather-app/src/components/search/search.js
@@ -1,24 +1,32 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import axios from "axios";
 
-const Search = (onSearchChange) => {
-
+const Search = ({ onSearchChange }) => {
   const [search, setSearch] = useState(null);
 
-  const handleOnChange = (searchData) => {
+  const handleOnChange = async (searchData) => {
     setSearch(searchData);
     onSearchChange(searchData);
-    
-  }
-  
+
+    // Fetch weather data based on the searched city
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${searchData}&appid=YOUR_API_KEY`
+    );
+
+    // Process the weather data here and update the state or pass it to the parent component
+    const weatherData = response.data;
+    console.log(weatherData);
+  };
+
   return (
     <AsyncPaginate
-    placeholder="Search for city"
-    debounceTimeout={600}
-    value={search}
-    onChange={handleOnChange}
+      placeholder="Search for city"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
     />
-  )
-}
+  );
+};
 
 export default Search;
