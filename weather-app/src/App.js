fe63b4265d2293b48dc715e46b861eb251f4c973
srewashimondal <67836280+srewashimondal@users.noreply.github.com>
2023-07-14 15:10:19 -0400

    Update App.js

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,17 +1,29 @@
-import './App.css';
-import Search from './components/search/search.js';
+import "./App.css";
+import Search from "./components/search/search.js";
+import { useState } from "react";
+import axios from "axios";
+import WeatherCard from "./components/WeatherCard";
 
 function App() {
+  const [weatherData, setWeatherData] = useState(null);
 
-  const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
-  }
-  
+  const handleOnSearchChange = async (searchData) => {
+    // Fetch weather data based on the searched city
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${searchData}&appid=YOUR_API_KEY`
+    );
+
+    // Process the weather data here and update the state
+    const weatherData = response.data;
+    setWeatherData(weatherData);
+  };
+
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
+      {weatherData && <WeatherCard weatherData={weatherData} />}
     </div>
   );
 }
 
-  export default App;
+export default App;
