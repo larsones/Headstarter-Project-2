74a0a129a968d450d8fca344c2fce6f9c7ec2e7d
srewashimondal <67836280+srewashimondal@users.noreply.github.com>
2023-07-14 11:36:55 -0400

    Update App.js

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,9 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from "react";
-import Search from "./components/search/search";
-import CurrentWeather from "./components/current-weather/current-weather";
-import Forecast from "./components/forecast/forecast";
+import Search from "./search";
+import CurrentWeather from "./current-weather";
+import Forecast from "./forecast";
 import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
 import "./App.css";
 
