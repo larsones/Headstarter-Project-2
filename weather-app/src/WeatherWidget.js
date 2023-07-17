d6f4df27e3db98f1d756da4a48f050ec40ab9914
srewashimondal <67836280+srewashimondal@users.noreply.github.com>
2023-07-17 10:46:59 -0400

    Update WeatherWidget.js

--- weather-app/src/WeatherWidget.js
+++ weather-app/src/WeatherWidget.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
-const WeatherWidget = ({ city }) => {
+const WeatherWidget = ({ weatherData, city }) => {
+  const { location, current } = weatherData;
+
   return (
     <div className="widget">
       <h2>Weather in {city}</h2>
-      {/* Weather data will go here */}
+      <p>Temperature: {current.temp_c}°C</p>
+      <p>Condition: {current.condition.text}</p>
+      <p>Humidity: {current.humidity}%</p>
+      <p>Wind: {current.wind_kph} km/h</p>
+      {/* Additional weather data can be displayed here */}
     </div>
   );
 }
