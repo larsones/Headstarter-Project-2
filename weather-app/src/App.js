aaf7c6c35345608956ab60431a7f732f2125ba82
srewashimondal <67836280+srewashimondal@users.noreply.github.com>
2023-07-17 19:26:38 -0400

    Update App.js

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import SearchBar from './SearchBar';
 import WeatherWidget from './WeatherWidget';
 import HourlyForecast from './HourlyForecast';
@@ -6,39 +6,13 @@ import WeeklyForecast from './WeeklyForecast';
 import AirQuality from './AirQuality';
 import './App.css';
 
-
 const App = () => {
   const [city, setCity] = useState('');
-  const apiKey = 'e7fc081df850c97f252bf1c3af358d51';
-  const [weatherData, setWeatherData] = useState([{}]);
-  const [newsData, setNewsData] = useState([]);
-
 
   const handleCitySearch = (cityName) => {
     setCity(cityName);
   };
 
-
-  useEffect(() => {
-  // Fetch news data when the city changes
-  const fetchNewsData = async () => {
-    try {
-      const response = await fetch(
-        `https://newsapi.org/v2/everything?q=${city}&sortBy=publishedAt&apiKey=1341a5014eae407c91aad0743e24edcd&pageSize=5`
-      );
-      const data = await response.json();
-      setNewsData(data.articles);
-    } catch (error) {
-      console.error('Error fetching news data:', error);
-    }
-  };
-
-
-  if (city) {
-    fetchNewsData();
-  }
-}, [city]);
-  
   return (
     <div className="App">
       <h1 className="text">
