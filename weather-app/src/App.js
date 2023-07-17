5d6169399212c38e8c63dfb61ed4a3cec7ed439d
srewashimondal <67836280+srewashimondal@users.noreply.github.com>
2023-07-17 19:07:51 -0400

    Update App.js

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -6,50 +6,39 @@ import WeeklyForecast from './WeeklyForecast';
 import AirQuality from './AirQuality';
 import './App.css';
 
+
 const App = () => {
   const [city, setCity] = useState('');
   const apiKey = 'e7fc081df850c97f252bf1c3af358d51';
   const [weatherData, setWeatherData] = useState([{}]);
   const [newsData, setNewsData] = useState([]);
 
+
   const handleCitySearch = (cityName) => {
     setCity(cityName);
   };
 
+
   useEffect(() => {
-    // Fetch news data when the city changes
-    const fetchNewsData = async () => {
-      try {
-        const response = await fetch(
-          `https://newsapi.org/v2/everything?q=${city}&sortBy=publishedAt&apiKey=YOUR_API_KEY&pageSize=100`
-        );
-        const data = await response.json();
-        const uniqueArticles = getUniqueArticles(data.articles.slice(0, 100));
-        setNewsData(uniqueArticles.slice(0, 5)); // Limit the articles to 5
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    if (city) {
-      fetchNewsData();
+  // Fetch news data when the city changes
+  const fetchNewsData = async () => {
+    try {
+      const response = await fetch(
+        `https://newsapi.org/v2/everything?q=${city}&sortBy=publishedAt&apiKey=1341a5014eae407c91aad0743e24edcd&pageSize=5`
+      );
+      const data = await response.json();
+      setNewsData(data.articles);
+    } catch (error) {
+      console.error('Error fetching news data:', error);
     }
-  }, [city]);
+  };
 
-  function getUniqueArticles(articles) {
-    const uniqueArticles = [];
-    const articleUrls = new Set();
 
-    for (const article of articles) {
-      if (!articleUrls.has(article.url)) {
-        uniqueArticles.push(article);
-        articleUrls.add(article.url);
-      }
-    }
-
-    return uniqueArticles;
+  if (city) {
+    fetchNewsData();
   }
-
+}, [city]);
+  
   return (
     <div className="App">
       <h1 className="text">
@@ -63,7 +52,7 @@ const App = () => {
         <AirQuality city={city} />
       </div>
       <div className="newsWidget">
-        <h1>News</h1>
+      <h1>    News    </h1>
         {newsData.map((article) => (
           <div key={article.title} className="article">
             <img src={article.urlToImage} alt={article.title} />
@@ -78,5 +67,6 @@ const App = () => {
     </div>
   );
 };
+
 
 export default App;
