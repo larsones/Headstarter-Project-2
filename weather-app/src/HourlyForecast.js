b72c61b6242b24b8f57c905039c9803cf46e6475
srewashimondal <67836280+srewashimondal@users.noreply.github.com>
2023-07-17 19:54:54 -0400

    Update HourlyForecast.js

--- weather-app/src/HourlyForecast.js
+++ weather-app/src/HourlyForecast.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const HourlyForecast = ({ city }) => {
+  const [hourlyForecast, setHourlyForecast] = useState([]);
+
+  useEffect(() => {
+    const fetchHourlyForecastData = async () => {
+      try {
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=e7fc081df850c97f252bf1c3af358d51`
+        );
+        const data = await response.json();
+        const transformedData = data.list.map((hour) => ({
+          time: hour.dt_txt,
+          temperature: hour.main.temp,
+          weather: hour.weather[0].description,
+        }));
+        setHourlyForecast(transformedData);
+      } catch (error) {
+        console.error('Error fetching hourly forecast data:', error);
+      }
+    };
+
+    if (city) {
+      fetchHourlyForecastData();
+    }
+  }, [city]);
+
   return (
     <div className="widget">
       <h2>Hourly Forecast in {city}</h2>
-      {/* Forecast data will go here */}
+      {hourlyForecast.map((hour) => (
+        <div key={hour.time} className="hour">
+          <p>Time: {hour.time}</p>
+          <p>Temperature: {hour.temperature} °C</p>
+          <p>Weather: {hour.weather}</p>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default HourlyForecast;
+
