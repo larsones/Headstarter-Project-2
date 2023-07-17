538389f94bad36cb23e1ed009e7a087fe4ebe8a2
srewashimondal <67836280+srewashimondal@users.noreply.github.com>
2023-07-17 18:33:29 -0400

    Update App.js

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
 import WeatherWidget from './WeatherWidget';
 import HourlyForecast from './HourlyForecast';
@@ -8,28 +8,59 @@ import './App.css';
 
 const App = () => {
   const [city, setCity] = useState('');
-  const apiKey = 'e7fc081df850c97f252bf1c3af358d51'
-  const [weatherData, setWeatherData] = useState([{}])
+  const apiKey = 'YOUR_API_KEY';
+  const [weatherData, setWeatherData] = useState([{}]);
+  const [newsData, setNewsData] = useState([]);
+
   const handleCitySearch = (cityName) => {
     setCity(cityName);
   };
 
+  useEffect(() => {
+    // Fetch news data when the city changes
+    const fetchNewsData = async () => {
+      try {
+        const response = await fetch(
+          `https://newsapi.org/v2/everything?q=${city}&sortBy=publishedAt&apiKey=${apiKey}&pageSize=5`
+        );
+        const data = await response.json();
+        setNewsData(data.articles);
+      } catch (error) {
+        console.error('Error fetching news data:', error);
+      }
+    };
+
+    if (city) {
+      fetchNewsData();
+    }
+  }, [city, apiKey]);
+
   return (
     <div className="App">
-
-      <h1 className='text'>Welcome!  Please Enter a Location to get current news and weather.</h1>
-      <SearchBar onCitySearch={handleCitySearch} className='search'/>
+      <h1 className="text">
+        Welcome! Please enter a location to get current news and weather.
+      </h1>
+      <SearchBar onCitySearch={handleCitySearch} className="search" />
       <div className="widgets">
         <WeatherWidget city={city} />
         <HourlyForecast city={city} />
         <WeeklyForecast city={city} />
         <AirQuality city={city} />
       </div>
-      <div className='newsWidget'>
-        
+      <div className="newsWidget">
+        {newsData.map((article) => (
+          <div key={article.title} className="article">
+            <img src={article.urlToImage} alt={article.title} />
+            <h2>{article.title}</h2>
+            <p>{article.description}</p>
+            <a href={article.url} target="_blank" rel="noopener noreferrer">
+              Read More
+            </a>
+          </div>
+        ))}
       </div>
     </div>
   );
-}
+};
 
 export default App;
