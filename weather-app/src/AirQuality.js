02d724357a67eaa58adac0983bd2ebe4db84a313
srewashimondal <67836280+srewashimondal@users.noreply.github.com>
2023-07-17 23:08:16 -0400

    Update AirQuality.js

--- weather-app/src/AirQuality.js
+++ weather-app/src/AirQuality.js
@@ -1,50 +1,68 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 
 const AirQuality = ({ city }) => {
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
   const [airQualityData, setAirQualityData] = useState(null);
-  const [error, setError] = useState(false);
 
   useEffect(() => {
-    const fetchAirQualityData = async () => {
+    const fetchCoordinates = async () => {
       try {
-        const response = await axios.get(
-          `https://api.airvisual.com/v2/city?city=${encodeURIComponent(city)}&key=7e56aae3-f67b-438f-9c06-b5aba522e089`
+        const geocodingResponse = await fetch(
+          `https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(
+            city
+          )}&key=5e96988d62de4b52aadbf5c51a1b8bef`
         );
-        const data = response.data;
-        console.log('API response:', data);
-        if (data.status === 'success') {
-          setAirQualityData(data);
-        } else {
-          setError(true);
+        const geocodingData = await geocodingResponse.json();
+        if (geocodingData.results.length > 0) {
+          const { lat, lng } = geocodingData.results[0].geometry;
+          setLatitude(lat);
+          setLongitude(lng);
         }
       } catch (error) {
-        console.error('Error fetching air quality data:', error);
-        setError(true);
+        console.error('Error fetching coordinates:', error);
       }
     };
 
     if (city) {
-      fetchAirQualityData();
+      fetchCoordinates();
     }
   }, [city]);
+
+  useEffect(() => {
+    const fetchAirQualityData = async () => {
+      try {
+        if (latitude && longitude) {
+          const airVisualResponse = await fetch(
+            `https://api.airvisual.com/v2/nearest_city?lat=${latitude}&lon=${longitude}&key=7e56aae3-f67b-438f-9c06-b5aba522e089`
+          );
+          const airVisualData = await airVisualResponse.json();
+          setAirQualityData(airVisualData);
+        }
+      } catch (error) {
+        console.error('Error fetching air quality data:', error);
+      }
+    };
+
+    if (latitude && longitude) {
+      fetchAirQualityData();
+    }
+  }, [latitude, longitude]);
 
   return (
     <div className="widget">
       <h2>Air Quality in {city}</h2>
-      {error ? (
-        <p>Error fetching air quality data.</p>
+      {airQualityData && airQualityData.status === 'success' ? (
+        <div>
+          <p>PM2.5: {airQualityData.data.current.pollution.pm25}</p>
+          <p>PM10: {airQualityData.data.current.pollution.pm10}</p>
+          <p>O3: {airQualityData.data.current.pollution.o3}</p>
+          <p>NO2: {airQualityData.data.current.pollution.no2}</p>
+          <p>SO2: {airQualityData.data.current.pollution.so2}</p>
+          <p>CO: {airQualityData.data.current.pollution.co}</p>
+        </div>
       ) : (
-        <>
-          {airQualityData ? (
-            <>
-              <p>Current Air Quality Index: {airQualityData.data.current.pollution.aqius}</p>
-              <p>Main Pollutant: {airQualityData.data.current.pollution.mainus}</p>
-            </>
-          ) : (
-            <p>No air quality data available.</p>
-          )}
-        </>
+        <p>No air quality data available.</p>
       )}
     </div>
   );
