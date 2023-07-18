3fbc9d080879f94f5f9e19e5f98ba152c36123e5
srewashimondal <67836280+srewashimondal@users.noreply.github.com>
2023-07-17 22:14:30 -0400

    Update AirQuality.js

--- weather-app/src/AirQuality.js
+++ weather-app/src/AirQuality.js
@@ -1,14 +1,52 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
 
 const AirQuality = ({ city }) => {
+  const [airQualityData, setAirQualityData] = useState(null);
+
+  useEffect(() => {
+    const fetchAirQualityData = async () => {
+      try {
+        const response = await fetch(
+          `http://api.openweathermap.org/data/2.5/air_pollution/forecast?appid=e7fc081df850c97f252bf1c3af358d51&q=${encodeURIComponent(city)}`
+        );
+        const data = await response.json();
+        setAirQualityData(data);
+      } catch (error) {
+        console.error('Error fetching air quality data:', error);
+      }
+    };
+
+    if (city) {
+      fetchAirQualityData();
+    }
+  }, [city]);
+
+  const formatTimestamp = (timestamp) => {
+    const date = new Date(timestamp * 1000);
+    const options = { weekday: 'long', month: 'long', day: 'numeric' };
+    return date.toLocaleDateString(undefined, options);
+  };
+
+  const renderAirQuality = () => {
+    if (!airQualityData || !airQualityData.list) {
+      return <p>No air quality data available</p>;
+    }
+
+    return airQualityData.list.map((entry) => (
+      <div key={entry.dt} className="air-quality-entry">
+        <p>{formatTimestamp(entry.dt)}</p>
+        <p>Air Quality Index (AQI): {entry.main.aqi}</p>
+      </div>
+    ));
+  };
+
   return (
     <div className="widget">
       <h2>Air Quality in {city}</h2>
-      {/* Air quality data will go here */}
+      {renderAirQuality()}
     </div>
   );
-}
-
+};
 
 export default AirQuality;
+
