0c851dfc9bc215df3a3c506f95d7616c1d7bee45
srewashimondal <67836280+srewashimondal@users.noreply.github.com>
2023-07-17 20:15:04 -0400

    Update WeeklyForecast.js

--- weather-app/src/WeeklyForecast.js
+++ weather-app/src/WeeklyForecast.js
@@ -10,11 +10,7 @@ const WeeklyForecast = ({ city }) => {
           `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=e7fc081df850c97f252bf1c3af358d51`
         );
         const data = await response.json();
-        const transformedData = data.list.map((day) => ({
-          date: formatDate(day.dt_txt),
-          temperature: convertKelvinToCelsius(day.main.temp),
-          weather: day.weather[0].description,
-        }));
+        const transformedData = transformForecastData(data.list);
         setWeeklyForecast(transformedData);
       } catch (error) {
         console.error('Error fetching weekly forecast data:', error);
@@ -26,6 +22,39 @@ const WeeklyForecast = ({ city }) => {
     }
   }, [city]);
 
+  const transformForecastData = (forecastData) => {
+    const dailyData = {};
+    const transformedData = [];
+
+    forecastData.forEach((item) => {
+      const date = item.dt_txt.split(' ')[0];
+
+      if (!dailyData[date]) {
+        dailyData[date] = {
+          date: formatDate(item.dt_txt),
+          temperatures: [],
+          weather: [],
+        };
+      }
+
+      dailyData[date].temperatures.push(item.main.temp);
+      dailyData[date].weather.push(item.weather[0].description);
+    });
+
+    Object.values(dailyData).forEach((daily) => {
+      const averageTemperature = calculateAverage(daily.temperatures);
+      const weather = daily.weather[0];
+
+      transformedData.push({
+        date: daily.date,
+        temperature: convertKelvinToCelsius(averageTemperature),
+        weather,
+      });
+    });
+
+    return transformedData;
+  };
+
   const formatDate = (dateString) => {
     const options = { weekday: 'long', month: 'long', day: 'numeric' };
     const date = new Date(dateString).toLocaleDateString([], options);
@@ -34,6 +63,11 @@ const WeeklyForecast = ({ city }) => {
 
   const convertKelvinToCelsius = (temperature) => {
     return Math.round(temperature - 273.15);
+  };
+
+  const calculateAverage = (values) => {
+    const sum = values.reduce((acc, value) => acc + value, 0);
+    return sum / values.length;
   };
 
   return (
