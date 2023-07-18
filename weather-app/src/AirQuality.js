aa7aeb525597fea1ced248495d6bbada6c07a260
srewashimondal <67836280+srewashimondal@users.noreply.github.com>
2023-07-17 22:31:53 -0400

    Update AirQuality.js

--- weather-app/src/AirQuality.js
+++ weather-app/src/AirQuality.js
@@ -7,9 +7,7 @@ const AirQuality = ({ city }) => {
     const fetchAirQualityData = async () => {
       try {
         const response = await fetch(
-          `https://api.openaq.org/v1/measurements?country_id=US&city=${encodeURIComponent(
-            city
-          )}&limit=1&parameter=pm25&sort=desc`
+          `https://api.openaq.org/v1/measurements?country_id=US&city=${encodeURIComponent(city)}&limit=1&parameter=pm25&sort=desc`
         );
         const data = await response.json();
         setAirQualityData(data);
@@ -23,24 +21,17 @@ const AirQuality = ({ city }) => {
     }
   }, [city]);
 
-  const renderAirQuality = () => {
-    if (!airQualityData || airQualityData.results.length === 0) {
-      return <p>No air quality data available for {city}</p>;
-    }
-
-    const { value, unit, date } = airQualityData.results[0];
-    return (
-      <div className="air-quality-entry">
-        <p>PM2.5: {value} {unit}</p>
-        <p>Last Updated: {date.local}</p>
-      </div>
-    );
-  };
-
   return (
     <div className="widget">
       <h2>Air Quality in {city}</h2>
-      {renderAirQuality()}
+      {airQualityData && airQualityData.results ? (
+        <div>
+          <p>PM2.5: {airQualityData.results[0].value} µg/m³</p>
+          <p>Last Updated: {airQualityData.results[0].date.local}</p>
+        </div>
+      ) : (
+        <p>No air quality data available for {city}.</p>
+      )}
     </div>
   );
 };
