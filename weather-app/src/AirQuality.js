3e9e5b40da23bb0983b3e7903f7faa4a7f755d07
srewashimondal <67836280+srewashimondal@users.noreply.github.com>
2023-07-17 22:33:22 -0400

    Update AirQuality.js

--- weather-app/src/AirQuality.js
+++ weather-app/src/AirQuality.js
@@ -7,7 +7,7 @@ const AirQuality = ({ city }) => {
     const fetchAirQualityData = async () => {
       try {
         const response = await fetch(
-          `https://api.openaq.org/v1/measurements?country_id=US&city=${encodeURIComponent(city)}&limit=1&parameter=pm25&sort=desc`
+          `https://api.openaq.org/v2/latest?country_id=US&city=${encodeURIComponent(city)}&parameter=pm25`
         );
         const data = await response.json();
         setAirQualityData(data);
@@ -26,8 +26,8 @@ const AirQuality = ({ city }) => {
       <h2>Air Quality in {city}</h2>
       {airQualityData && airQualityData.results ? (
         <div>
-          <p>PM2.5: {airQualityData.results[0].value} µg/m³</p>
-          <p>Last Updated: {airQualityData.results[0].date.local}</p>
+          <p>PM2.5: {airQualityData.results[0].measurements[0].value} {airQualityData.results[0].measurements[0].unit}</p>
+          <p>Last Updated: {airQualityData.results[0].measurements[0].lastUpdated}</p>
         </div>
       ) : (
         <p>No air quality data available for {city}.</p>
